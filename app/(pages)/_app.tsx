fbcca0e40653acd3c7d97689919db04e313a9cbb
rafliafriza12 <rafliafriza90@gmail.com>
2024-12-18 22:44:15 +0700

    feat: add MUI config

---
+++ app/(pages)/_app.tsx
@@ -0,0 +1,26 @@
+// pages/_app.tsx
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { AppProps } from "next/app";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#dc004e",
+    },
+  },
+  typography: {
+    fontFamily: "Roboto, Arial, sans-serif",
+  },
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
+import { Button, Typography } from "@mui/material";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        Hello, Next.js with Material UI!
+      </Typography>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
+    </div>
+  );
 }
