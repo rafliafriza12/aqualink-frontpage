90e87fcc18304d256e586db2061240fb170aa563
rafliafriza12 <rafliafriza90@gmail.com>
2024-12-19 16:33:17 +0700

    feat: add desktop interface for home page

--- app/layouts/UserLayout.tsx
+++ app/layouts/UserLayout.tsx
@@ -7,10 +7,12 @@ import { useRouter } from "next/navigation";
 import Navbar from "../components/navbar/Index";
 import { CardHeader } from "@mui/material";
 import Image from "next/image";
+import { IsDesktop } from "../hooks";
 
 const UserLayout: React.FC<UserLayoutProps> = ({ children }) => {
   const navigation = useRouter();
   const auth = useAppSelector((state) => state.auth);
+  const isDesktop = IsDesktop();
 
   if (!auth.isAuthenticated) {
     navigation.replace("/auth/login");
@@ -20,19 +22,25 @@ const UserLayout: React.FC<UserLayoutProps> = ({ children }) => {
   return (
     <>
       <CssBaseline />
-      <Container maxWidth="lg">
-        <CardHeader
-          sx={{ pl: 0, py: 4 }}
-          title={<Typography variant="body2">23°C Banda Aceh</Typography>}
-          action={
-            <Image
-              src={"/assets/logo/logo-aqualink.png"}
-              alt="AquaLink"
-              width={35}
-              height={35}
-            />
-          }
-        />
+      <Container maxWidth="xl" sx={{ pt: isDesktop ? 15 : 0 }}>
+        {!isDesktop && (
+          <CardHeader
+            sx={{ pl: 0, py: 4 }}
+            title={
+              <Typography variant={`${isDesktop ? "body1" : "body2"}`}>
+                23°C Banda Aceh
+              </Typography>
+            }
+            action={
+              <Image
+                src={"/assets/logo/logo-aqualink.png"}
+                alt="AquaLink"
+                width={isDesktop ? 55 : 35}
+                height={isDesktop ? 55 : 35}
+              />
+            }
+          />
+        )}
         {children}
         <Navbar />
       </Container>
