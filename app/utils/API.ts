b5f9419f9bdf025806cb8c6597313ff98188b56d
rafliafriza12 <rafliafriza90@gmail.com>
2025-08-03 13:25:08 +0700

    feat: add handling for expired token

--- app/utils/API.ts
+++ app/utils/API.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
-
+import { store } from "../store";
+import { logout } from "../store/slices/authSlice";
 // Membuat instance API dengan konfigurasi dasar
 const API = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}`,
@@ -21,6 +22,11 @@ API.interceptors.response.use(
     return response;
   },
   (error: AxiosError): Promise<AxiosError> => {
+    const status = error?.response?.status;
+
+    if (status === 401 || status === 403) {
+      store.dispatch(logout());
+    }
     return Promise.reject(error);
   }
 );
